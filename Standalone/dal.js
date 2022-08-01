9f47e5841aafb03ec46744ced82f016aba476983
slumpbuster <djsellers17@gmail.com>
2022-08-01 17:21:12 -0400

    setting up to push to Heroku

--- Standalone/dal.js
+++ Standalone/dal.js
@@ -2,18 +2,17 @@ const MongoClient = require('mongodb').MongoClient;
 const { v4: uuid_v4 } = require('uuid');
 require('dotenv').config();
 const url = process.env.MONGO_URL;
-let db = null;
-
-MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
-  db = client.db('myproject');
-});
+const db = process.env.MONGO_DB;
+const client = new MongoClient(url, {useUnifiedTopology: true});
 
 const create = (authId, name, email, password) => {
   return new Promise((resolve, reject) => {
-    const collection = db.collection('users');
-    let doc = {authId, name, email, password, role: 'user', balance: 0, status: 'active', failedAttempts: 0};
-    collection.insertOne(doc, {w:1}, (err, result) => {
-      err ? reject(err) : resolve(doc);
+    client.connect((err) => {
+      const collection = client.db(db).collection('users');
+      let doc = {authId, name, email, password, role: 'user', balance: 0, status: 'active', failedAttempts: 0};
+      collection.insertOne(doc, {w:1}, (err, result) => {
+        err ? reject(err) : resolve(doc);
+      });
     });
   });
 }
@@ -25,19 +24,23 @@ const transaction = async (authId, transaction, balance) => {
   let newTransaction = {transId: newTransId, date: new Date().toISOString().slice(0, 10), starting: user.balance, transaction: transaction, ending: balance, image: false};
   transactions = [...transactions, newTransaction];
   return new Promise((resolve, reject) => {
-    const collection = db.collection('users');
-    collection.updateOne({authId: authId}, {$set: {balance: balance, transactions: transactions}}, {upsert: true})
-      .then((doc) => resolve({doc, newTransaction: [newTransaction]}))
-      .catch((err) => reject(err));
+    client.connect((err) => {
+      const collection = client.db(db).collection('users');
+      collection.updateOne({authId: authId}, {$set: {balance: balance, transactions: transactions}}, {upsert: true})
+        .then((doc) => resolve({doc, newTransaction: [newTransaction]}))
+        .catch((err) => reject(err));
+    });
   });
 }
 
 const updatefailedAttempts = (authId, failedAttempts) => {
   return new Promise((resolve, reject) => {
-    const collection = db.collection('users');
-    collection.updateOne({authId: authId}, {$set: {failedAttempts: failedAttempts, status: failedAttempts>=6 ? 'disabled' : 'active'}}, {upsert: true})
-      .then((doc) => resolve({doc}))
-      .catch((err) => reject(err));
+    client.connect((err) => {
+      const collection = client.db(db).collection('users');
+      collection.updateOne({authId: authId}, {$set: {failedAttempts: failedAttempts, status: failedAttempts>=6 ? 'disabled' : 'active'}}, {upsert: true})
+        .then((doc) => resolve({doc}))
+        .catch((err) => reject(err));
+    });
   });
 }
 
@@ -51,39 +54,44 @@ const image = async (authId, transId) => {
     }
   }
   return new Promise((resolve, reject) => {
-    const collection = db.collection('users');
-    collection.updateOne({authId: authId}, {$set: {transactions: transactions}}, {upsert: true})
-      .then((doc) => resolve({doc}))
-      .catch((err) => reject(err));
+    client.connect((err) => {
+      const collection = client.db(db).collection('users');
+      collection.updateOne({authId: authId}, {$set: {transactions: transactions}}, {upsert: true})
+        .then((doc) => resolve({doc}))
+        .catch((err) => reject(err));
+    });
   });
 }
 
 const all = () => {
   return new Promise((resolve, reject) => {
-    const users = db
-      .collection('users')
-      .find({})
-      .toArray((err, docs) => {
-        err ? reject(err) : resolve(docs);
+    client.connect((err) => {
+      const collection = client.db(db).collection('users');
+      collection.find({})
+        .toArray((err, docs) => {
+          err ? reject(err) : resolve(docs);
+      });
     });
   });
 }
 const findOne = (email) => {
   return new Promise((resolve, reject) => {
-    const users = db
-      .collection('users')
-      .findOne({email: email})
-      .then((doc) => resolve(doc))
-      .catch((err) => reject(err));
+    client.connect((err) => {
+      const collection = client.db(db).collection('users');
+      collection.findOne({email: email})
+        .then((doc) => resolve(doc))
+        .catch((err) => reject(err));
+    });
   });
 }
 const findAuth = (authId) => {
   return new Promise((resolve, reject) => {
-    const users = db
-      .collection('users')
-      .findOne({authId: authId})
-      .then((doc) => resolve(doc))
-      .catch((err) => reject(err));
+    client.connect((err) => {
+      const collection = client.db(db).collection('users');
+      collection.findOne({authId: authId})
+        .then((doc) => resolve(doc))
+        .catch((err) => reject(err));
+    });
   });
 }
 
